2f5bb0aa1d177d8accacb9db4750e68b8ae7a76a
echen10 <echen10@stuy.edu>
2017-06-06 12:18:19 -0400

    Removed abstract class, putting all pieces into the Piece class

--- Piece.java
+++ Piece.java
@@ -1,12 +1,60 @@
-abstract class Piece {
+public class Piece {
     private int[][] coords;
     private int orientation;
     private boolean moving;
     
-    abstract void rotate( int direction );
+    public LPiece( int type ) {
+        orientation = type;
+	coords = new int[4][];
+	if ( type == 0 ) { // LPiece
+	    coords[0] = new int[] { 1,3 };
+	    coords[1] = new int[] { 1,2 };
+	    coords[2] = new int[] { 1,1 };
+	    coords[3] = new int[] { 2,1 };
+	}
+        else if ( type == 1 ) { // JPiece
+	    coords[0] = new int[] { 1,1 };
+	    coords[1] = new int[] { 2,1 };
+	    coords[2] = new int[] { 2,2 };
+	    coords[3] = new int[] { 2,3 };
+	}
+	else if ( type == 2 ) { // IPiece
+	    coords[0] = new int[] { 1,3 };
+	    coords[1] = new int[] { 1,2 };
+	    coords[2] = new int[] { 1,1 };
+	    coords[3] = new int[] { 1,0 };
+	}
+	else if ( type == 3 ) { // TPiece
+	    coords[0] = new int[] { 0,1 };
+	    coords[1] = new int[] { 1,1 };
+	    coords[2] = new int[] { 1,2 };
+	    coords[3] = new int[] { 2,1 };
+	}
+	else if ( type == 4 ) { // OPiece
+	    coords[0] = new int[] { 1,1 };
+	    coords[1] = new int[] { 2,1 };
+	    coords[2] = new int[] { 1,2 };
+	    coords[3] = new int[] { 2,2 };
+	}
+	else if ( type == 5 ) { // ZPiece
+	    coords[0] = new int[] { 0,2 };
+	    coords[1] = new int[] { 1,2 };
+	    coords[2] = new int[] { 1,1 };
+	    coords[3] = new int[] { 2,1 };
+	}
+	else { //SPiece
+	    coords[0] = new int[] { 0,2 };
+	    coords[1] = new int[] { 1,2 };
+	    coords[2] = new int[] { 1,1 };
+	    coords[3] = new int[] { 2,2 };
+	}
+    }
 
     public int[][] getCoords() {
 	return coords;
+    }
+
+    public void rotate( int direction ){
     }
 
     public void move(int dx, int dy) {
